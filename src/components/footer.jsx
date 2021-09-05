12556254b209da4f5ed3a673c4581ad9384d03d8
Krishen Mohan <krishenm@gmail.com>
2021-09-05 21:46:15 +0800

    set up basic multi route site

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div classname="footer">
+      <footer class="py-5 bg-dark fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Krishen Mohan 2021
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
