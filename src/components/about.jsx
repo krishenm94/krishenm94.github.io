d7826a48389899ba57b23fe098660aae69245545
Krishen Mohan <krishenm@gmail.com>
2021-09-05 22:12:00 +0800

    modify some stuff

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,10 +15,8 @@ function About() {
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              My attempt to learn web development married with providing myself
+              a platform to publish articles.
             </p>
           </div>
         </div>
